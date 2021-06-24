0fd426709038d6f383e2867fe48d41fb5324b277
William Faunce <EngineerFaunce@users.noreply.github.com>
2021-06-24 12:45:03 -0400

    Initial GUI application

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,135 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::fs::File;
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead};
 use std::path::Path;
 
-fn main() {
-    let fighter_list = Fighter::get_all_fighters();
-    let mut input = String::new();
+use iced::{
+    button, executor, scrollable, Align, Application, Button, Clipboard, Column, Command,
+    Container, Element, Length, Row, Scrollable, Settings, Space, Text,
+};
 
-    println!("SSBU Randomizer\n");
+fn main() -> iced::Result {
+    Randomizer::run(Settings::default())
+}
 
-    loop {
-        print!("Enter a command: ");
-        io::stdout().flush().expect("flush failed!");
+// Modeling the state of the application
+struct Randomizer {
+    generate: button::State,
+    list_area: scrollable::State,
+    fighter_list: Vec<Fighter>,
+}
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input.");
+// Defining the possible user interactions for the randomizer
+#[derive(Debug, Clone)]
+pub enum Message {
+    Generate,
+    Toggled(bool),
+}
 
-        match &input.trim()[..] {
-            "help" => {
-                println!("\nlist - view all fighters");
-                println!("gen, generate - generate a randomized list of fighters");
-                println!("exit - exit the program\n");
-            }
-            "list" => {
-                println!();
-                for fighter in &fighter_list {
-                    println!("{}", fighter.name);
+impl Application for Randomizer {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Randomizer, Command<Message>) {
+        (
+            Randomizer {
+                generate: button::State::new(),
+                list_area: scrollable::State::new(),
+                fighter_list: Fighter::load_fighters(),
+            },
+            Command::none(),
+        )
+    }
+
+    // Title of the application
+    fn title(&self) -> String {
+        String::from("SSBU Randomizer")
+    }
+
+    // Where we update the state of the application
+    fn update(&mut self, _message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
+        match _message {
+            Message::Generate => {
+                *self = Randomizer {
+                    generate: button::State::new(),
+                    list_area: scrollable::State::new(),
+                    fighter_list: Fighter::randomize_fighters(&self.fighter_list),
                 }
-                println!();
             }
-            "generate" | "gen" => {
-                let mut new_list = fighter_list.to_vec();
-                new_list.shuffle(&mut thread_rng());
-
-                println!();
-                for fighter in new_list {
-                    println!("{}", fighter.name);
-                }
-                println!();
-            }
-            "exit" => std::process::exit(0),
-            "" => continue,
-            &_ => println!("Unknown command. Type 'help' for a list of commands."),
+            Message::Toggled(_) => {}
         }
+        Command::none()
+    }
 
-        input = String::new();
+    // View of the application
+    fn view(&mut self) -> Element<Message> {
+        let content = match self {
+            Randomizer {
+                generate,
+                list_area,
+                fighter_list,
+            } => Column::new()
+                .spacing(50)
+                .align_items(Align::Center)
+                .push(
+                    Column::new()
+                        .max_width(600)
+                        .spacing(20)
+                        .align_items(Align::Center)
+                        .push(Text::new("Generate a randomized list of fighters!").size(20))
+                        .push(
+                            Button::new(generate, Text::new("Generate"))
+                                .on_press(Message::Generate)
+                                .padding(8)
+                                .style(style::Button::Primary),
+                        ),
+                )
+                .push(
+                    Column::new()
+                        .max_width(600)
+                        .max_height(300)
+                        .spacing(20)
+                        .align_items(Align::Center)
+                        .push(
+                            Scrollable::new(list_area)
+                                .padding(10)
+                                .width(Length::Units(500))
+                                .scrollbar_width(10)
+                                .push({
+                                    let mut container = Column::new();
+                                    for fighter in fighter_list {
+                                        container = container.push(
+                                            Row::new()
+                                                .push(
+                                                    Text::new(format!("{} -", &fighter.name))
+                                                        .size(20),
+                                                )
+                                                .push(
+                                                    Text::new(format!(" {} ", &fighter.series))
+                                                        .size(20),
+                                                )
+                                                // .push(Text::new(format!(
+                                                //     "- Is echo: {}",
+                                                //     fighter.is_echo.to_string()
+                                                // )))
+                                                .push(Space::with_height(Length::Units(30))),
+                                        );
+                                    }
+
+                                    container
+                                }),
+                        ),
+                ),
+        };
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
     }
 }
 
@@ -59,7 +142,7 @@ struct Fighter {
 
 impl Fighter {
     // Returns a list of all fighters from data file
-    fn get_all_fighters() -> Vec<Fighter> {
+    fn load_fighters() -> Vec<Fighter> {
         // The output is wrapped in a Result to allow matching on errors
         // Returns an Iterator to the Reader of the lines of the file.
         fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -87,5 +170,33 @@ impl Fighter {
             }
         }
         fighter_list
+    }
+
+    fn randomize_fighters(list: &Vec<Fighter>) -> Vec<Fighter> {
+        let mut new_list = list.to_vec();
+        new_list.shuffle(&mut thread_rng());
+        new_list
+    }
+}
+
+mod style {
+    use iced::{button, Background, Color, Vector};
+
+    pub enum Button {
+        Primary,
+    }
+
+    impl button::StyleSheet for Button {
+        fn active(&self) -> button::Style {
+            button::Style {
+                background: Some(Background::Color(match self {
+                    Button::Primary => Color::from_rgb(0.11, 0.42, 0.87),
+                })),
+                border_radius: 4.0,
+                shadow_offset: Vector::new(0.0, 0.0),
+                text_color: Color::WHITE,
+                ..button::Style::default()
+            }
+        }
     }
 }
